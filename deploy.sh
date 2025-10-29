5591071f5721645bcafc8d45b5863b28ccd7c6db
Simon Anders <sanders@fs.tum.de>
2025-10-29 16:33:51 +0100

    tidyverse intro

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,4 @@
 
 quarto render --cache
 rsync -r _site/ anders@papagei.bioquant.uni-heidelberg.de:www/dabio25
+# rsync -e "ssh -J appl1" -r _site/ anders@papagei.bioquant.uni-heidelberg.de:www/dabio25
